ca084758bc18d38832572aa6d298cd95c9d9b2f5
Kuleb1n <v_vpyt@mail.ru>
2023-04-28 22:38:33 +0300

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run()
+
+with app.test_request_context():
+    print(url_for('index'))
